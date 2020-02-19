3be1012291d097827971a1bb37e962ac6a294268
xxx <1026737316@qq.com>
2020-02-19 14:56:48 +0800

    add rooms plot contactphone

--- BackEnd/app/models.py
+++ BackEnd/app/models.py
@@ -9,11 +9,13 @@ class User(db.Model):
     __tablename__ = 'users'
     __table_args__ = {'mysql_engine': 'InnoDB'}  # 支持事务操作和外键
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(128), doc='账号', nullable=False)
-    password = db.Column(db.String(128), doc='账号密码', nullable=False)
+    oppenId = db.Column(db.Integer, doc='微信账号唯一标识', nullable=True)
+    username = db.Column(db.String(128), doc='用户名', nullable=False)
+    password = db.Column(db.String(128), doc='密码', nullable=False)
     phone = db.Column(db.String(20), doc='手机号码', nullable=False, unique=True)
     isAdmin = db.Column(db.Boolean, doc='是否管理员', default=False)
-    userRestrict = db.Column(db.String(32), doc='用户权限', nullable=False)
+    userRestrict = db.Column(db.String(32), doc='用户权限', nullable=True)
+    contain = db.Column(db.String(128), doc='现租的房子', nullable=True)
     description = db.Column(db.String(50), doc='说明', default='这个人很懒，什么也没留下', nullable=False)
     
 
@@ -25,15 +27,18 @@ class Room(db.Model):
     title = db.Column(db.String(255), doc='标题', nullable=False)
     position = db.Column(db.String(255), doc='位置', nullable=False)
     roomType = db.Column(db.String(128), doc='住房类型', nullable=False)
-    IsElevator = db.Column(db.Boolean, doc='是否有电梯', default=False)
+    isElevator = db.Column(db.Boolean, doc='是否有电梯', default=False)
     price = db.Column(db.Integer, doc='价格', nullable=False, default=1000)
     nearSubway = db.Column(db.String(128), doc='临近地铁', nullable=False)
     releaseTime = db.Column(db.DateTime, doc='发布日期时间', nullable=False)
     payType = db.Column(db.String(128), doc='支付方式', nullable=False)
     area = db.Column(db.Float, doc='房间面积', nullable=False)
-    Floor = db.Column(db.Integer, doc='楼层', nullable=False)
-    PicList = db.Column(db.LargeBinary, doc='图片列表', nullable=False)
+    floor = db.Column(db.Integer, doc='楼层', nullable=False)
+    plot = db.Column(db.String(255), doc='小区名字', nullable=False)
+    picList = db.Column(db.LargeBinary, doc='图片列表', nullable=False)
     supporting = db.Column(db.String(255), doc='配套设施', nullable=False)
+    contactPhone = db.Column(db.Integer, doc='联系电话', nullable=False)
+    contactWx = db.Column(db.String(255), doc='联系微信', nullable=False)
     description = db.Column(db.String(255), doc='说明', default='这个人很懒，什么也没留下')
     
 
